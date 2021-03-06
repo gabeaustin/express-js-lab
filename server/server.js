f45a96d0c5a0cba9af491de0eef3fa9e8a0a134a
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-06 04:03:37 -0600

    added bootstrap

--- server/server.js
+++ server/server.js
@@ -3,10 +3,17 @@ const path = require("path");
 let app = express();
 
 app.get("/", (req, res, next) => {
-    res.send("Hello from the web server side...");
+    res.send("Hello from xthe web server side...");
+});
+
+app.get("/", (req, res, next) => {
+    console.log(next);
+    res.send("helloworld");
 });
 
 app.use(express.static(path.join(__dirname, "../public")));
+
+app.use();
 
 // this is how to turn the server on
 app.listen(3000);
