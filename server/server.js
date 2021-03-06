992c3f9139d0d7fc51f50537da8bed810db890f3
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-06 14:51:25 -0600

    created middleware that used app.use

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const path = require("path");
 let app = express();
 
+app.use((req, res, next) => {
+    console.log(`Request URL: ${req.url}`);
+    next();
+});
+
 app.get("/", (req, res, next) => {
     res.send("Hello from xthe web server side...");
 });
@@ -13,7 +18,11 @@ app.get("/", (req, res, next) => {
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.use();
+// app.use('/user/:id', function (req, res, next) {
+//     console.log('Request URL:', req.originalUrl)
+//     next();
+// });
+
 
 // this is how to turn the server on
 app.listen(3000);
