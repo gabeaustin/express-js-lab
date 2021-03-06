b6af5782c0b358430d678c4a278c0cc020b7bbc8
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-06 15:05:18 -0600

    updated readme

--- server/server.js
+++ server/server.js
@@ -2,27 +2,20 @@ const express = require("express");
 const path = require("path");
 let app = express();
 
+// Number 5 
 app.use((req, res, next) => {
     console.log(`Request URL: ${req.url}`);
     next();
 });
 
+// Number 3 
 app.get("/", (req, res, next) => {
-    res.send("Hello from xthe web server side...");
+    res.send("Hello from the web server side...");
 });
 
-app.get("/", (req, res, next) => {
-    console.log(next);
-    res.send("helloworld");
-});
-
+// Number 5
 app.use(express.static(path.join(__dirname, "../public")));
 
-// app.use('/user/:id', function (req, res, next) {
-//     console.log('Request URL:', req.originalUrl)
-//     next();
-// });
-
-
-// this is how to turn the server on
+// Number 4
+/* this is how to turn the server on */
 app.listen(3000);
